95b6dbdf121d11040b5726efc1985eb9d10de3a9
Romain Lenzotti <rom.lenzotti@gmail.com>
2024-12-13 22:12:50 +0100

    chore: initial commit

---
+++ src/Server.ts
@@ -0,0 +1,43 @@
+import {join} from "path";
+import {Configuration} from "@tsed/di";
+import "@tsed/platform-log-middleware"; // remove this import if you don&#x27;t want log request
+import "@tsed/platform-express"; // /!\ keep this import
+import "@tsed/ajv";
+import {config} from "./config/index.js";
+import * as rest from "./controllers/rest/index.js";
+
+@Configuration({
+  ...config,
+  acceptMimes: ["application/json"],
+  httpPort: process.env.PORT || 8083,
+  httpsPort: false, // CHANGE
+  disableComponentsScan: true,
+  ajv: {
+    returnsCoercedValues: true
+  },
+  mount: {
+    "/rest": [
+      ...Object.values(rest)
+    ]
+  },
+  middlewares: [
+    "cors",
+    "cookie-parser",
+    "compression",
+    "method-override",
+    "json-parser",
+    { use: "urlencoded-parser", options: { extended: true }}
+  ],
+  views: {
+    root: join(process.cwd(), "../views"),
+    extensions: {
+      ejs: "ejs"
+    }
+  },
+  exclude: [
+    "**/*.spec.ts"
+  ]
+})
+export class Server {
+
+}
